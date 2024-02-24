6492691966a1dbe6067ba7a71dcc080686a985ef
sjglm <mattyglockhart@aol.com>
2024-02-24 12:34:16 +0000

    Added a go to top button using javascript

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const linkedinBtn = document.getElementById('linkedin-btn');
 const githubBtn = document.getElementById('github-btn');
 const cvBtn = document.getElementById('cv-btn');
+const goToTopBtn = document.getElementById('goToTopBtn');
 
 linkedinBtn.addEventListener('mouseenter', ()=>{
     linkedinBtn.style.backgroundColor = '#0056b3'
@@ -21,3 +22,31 @@ cvBtn.addEventListener('mouseenter', ()=>{
 cvBtn.addEventListener('mouseleave', () => {
     cvBtn.style.backgroundColor = '#630fff'; 
 });
+goToTopBtn.addEventListener('mouseenter', ()=>{
+    goToTopBtn.style.backgroundColor = '#dcdcdc'
+});
+goToTopBtn.addEventListener('mouseleave', () => {
+    goToTopBtn.style.backgroundColor = '#f4f4f4'; 
+});
+
+window.onscroll = function() {scrollFunction()};
+
+function goToTop()
+{
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+}
+
+function scrollFunction()
+{
+    if(document.body.scrollTop > 500 || document.documentElement.scrollTop > 500)
+    {
+        document.getElementById("goToTopBtn").style.display = "block";
+    }
+    else
+    {
+        document.getElementById("goToTopBtn").style.display = "none";
+    }
+}
